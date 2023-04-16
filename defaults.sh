47f271a793f8a628b9c0495497d667729a76d8f8
Bryce Hamrick <brycehamrick@gmail.com>
2023-04-16 12:44:23 -0400

    Update defaults.sh
    Updated

--- defaults.sh
+++ defaults.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-# Use scroll gesture with the Ctrl (^) modifier key to zoom
-echo "ℹ️  Enable Full Disk Access for the terminal and run the following command:"
-echo "defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true"
-echo
-
 # Disable the “are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Show hidden files and files extensions
+defaults write com.apple.finder AppleShowAllFiles TRUE
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Disable swipe navigation in Chrome
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
@@ -21,10 +20,6 @@ defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# New window location
-defaults write com.apple.finder NewWindowTarget -string "PfHm"
-defaults write com.apple.finder NewWindowTargetPath -string "file://$(cd ~; pwd)/"
 
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
@@ -39,33 +34,11 @@ defaults -currentHost write 'Apple Global Domain' com.apple.mouse.tapBehavior 1
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy name" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy name" ~/Library/Preferences/com.apple.finder.plist
 
-# Script Editor
-defaults write com.apple.ScriptEditor2 ApplePersistence -bool false
-defaults write com.apple.ScriptEditor2 DefaultLanguageType -int 1785946994
-
-# Hide welcome window
-defaults write com.apple.dt.Xcode XCShowWelcomeWindow 0
-
-# Music
-defaults write com.apple.Music dontAskDownloadArtwork 1
-defaults write com.apple.Music dontAskForPlaylistItemRemoval 1
-defaults write com.apple.Music dontAskForPlaylistRemoval 1
-defaults write com.apple.Music dontWarnWhenEditingMultiple 1
-defaults write com.apple.Music automaticallyDownloadArtwork 1
-
 # Safari
 defaults write com.apple.Safari IncludeInternalDebugMenu 1
 
-# Hide language menu
-defaults write com.apple.TextInputMenu visible -bool false
-
 # Feedback Assistant
 defaults write com.apple.appleseed.FeedbackAssistant.plist FBASuppressPrivacyNotice -bool true
-
-# Set languages and dot-based number separators
-defaults write .GlobalPreferences AppleLanguages -array "en-AU" "de-DE"
-defaults write .GlobalPreferences AppleLocale -string "en_DE"
-defaults write .GlobalPreferences AppleICUNumberSymbols -dict 0 '.' 1 ',' 10 '.' 17 ','
 
 # Menu extras
 defaults write com.apple.systemuiserver menuExtras -array \
@@ -74,13 +47,3 @@ defaults write com.apple.systemuiserver menuExtras -array \
   "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
   "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"
 killall SystemUIServer
-
-# Add login items
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Dropbox", path: "/Applications/Dropbox.app"}' > /dev/null
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Itsycal", path: "/Applications/Itsycal.app"}' > /dev/null
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "MacMediaKeyForwarder", path: "/Applications/MacMediaKeyForwarder.app"}' > /dev/null
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Übersicht", path: "/Applications/Übersicht.app"}' > /dev/null
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "xbar", path: "/Applications/xbar.app"}' > /dev/null
-
-# Screen Saver
-defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Aerial path ~/Library/Screen\ Savers/Aerial.saver type 0
