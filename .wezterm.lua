b0d0773da14b8955214be4b5280557e3e541df25
Bryce Hamrick <brycehamrick@gmail.com>
2025-09-06 13:41:14 -0600

    added wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require 'wezterm'
+
+return {
+  -- Font
+  font = wezterm.font_with_fallback {
+    "Hack Nerd Font Mono",
+    "SF Mono", -- native macOS system monospace
+  },
+  font_size = 14.0,
+
+  -- Colorscheme
+  color_scheme = "Afterglow",
+
+  -- Cursor
+  default_cursor_style = "SteadyBlock",
+
+  -- Tabs
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false, -- minimal tab bar, no fluff
+
+  -- Keybindings
+  keys = {
+    -- Cycle tabs with Cmd+Opt+Left/Right
+    { key = "LeftArrow", mods = "CMD|OPT", action = wezterm.action{ActivateTabRelative=-1} },
+    { key = "RightArrow", mods = "CMD|OPT", action = wezterm.action{ActivateTabRelative=1} },
+
+    -- Direct tab jumps with Cmd+Opt+[number]
+    { key = "1", mods = "CMD|OPT", action = wezterm.action{ActivateTab=0} },
+    { key = "2", mods = "CMD|OPT", action = wezterm.action{ActivateTab=1} },
+    { key = "3", mods = "CMD|OPT", action = wezterm.action{ActivateTab=2} },
+    { key = "4", mods = "CMD|OPT", action = wezterm.action{ActivateTab=3} },
+    { key = "5", mods = "CMD|OPT", action = wezterm.action{ActivateTab=4} },
+    { key = "6", mods = "CMD|OPT", action = wezterm.action{ActivateTab=5} },
+    { key = "7", mods = "CMD|OPT", action = wezterm.action{ActivateTab=6} },
+    { key = "8", mods = "CMD|OPT", action = wezterm.action{ActivateTab=7} },
+    { key = "9", mods = "CMD|OPT", action = wezterm.action{ActivateTab=8} },
+  },
+
+  -- Performance tweaks
+  animation_fps = 1,          -- disables cursor blink animations etc.
+  front_end = "WebGpu",       -- ensures GPU acceleration
+  warn_about_missing_glyphs = false, -- avoid annoying logs
+  audible_bell = "Disabled",  -- silence
+}
